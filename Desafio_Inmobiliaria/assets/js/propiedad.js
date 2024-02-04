48e484c2f40eaa704b82d690d19b3cc51293cfae
Sixto Obreque <sixto.obreque@gmail.com>
2024-02-04 00:04:08 -0300

    Se crea el Desafio_Inmobiliaria

---
+++ Desafio_Inmobiliaria/assets/js/propiedad.js
@@ -0,0 +1,75 @@
+// arreglo de propiedades en alquiler
+const propiedades_alquiler = [
+    {
+      nombre: 'Departamento de 3 dormitorios en sector Nueva Costanera.',
+      src: 'assets/css/img/img5.jpg',
+      descripcion: 'Ubicado en la mejor zona de Vitacura, a pasos del Parque Bicentenario y de Nueva Costanera, frente al colegio Las Ursulinas',
+      ubicacion: 'Vitacura, Region Metropolitana',
+      habitaciones: 3,
+      banos: 2,
+      costo: 1140000,
+      smoke: false,
+      pets: true
+    },
+    {
+      nombre: 'Departamento de 4 dormitorios en edificio Atlántico',
+      src: 'assets/css/img/img6.jpg',
+      descripcion: 'Ubicado en Valle del Mar, sector residencial.',
+      ubicacion: 'Ubicado en Valle del Mar, sector residencial.',
+      habitaciones: 4,
+      banos: 3,
+      costo: 1500000,
+      smoke: false,
+      pets: true
+    },
+    {
+      nombre: 'Departamento Andalué para arriendo',
+      src: 'assets/css/img/img7.jpg',
+      descripcion: 'Departamento un dormitorio con vista a la piscina y bosque nativo, Condominio ubicado en hermoso sector residencial',
+      Ubicado: 'Andalué, San Pedro de La Paz, Region del Bio Bio',
+      habitaciones: 1,
+      banos: 1,
+      costo: 1500000,
+      smoke: false,
+      pets: true
+    },
+    ]
+  
+  // arreglo de propiedades en venta
+  const propiedades_venta =[
+    {
+      nombre: 'Apartamento de lujo con vista al mar',
+      src: 'assets/css/img/img2.jpg',
+      descripcion: '4 dormitorios en suite en 250 mts2 con una vista imponente a la Playa Mansa, así como al bosque de Punta del Este',
+      ubicacion: 'Localizado vista a la Playa Mansa y hacia el puerto de Punta',
+      habitaciones: 4,
+      banos: 2,
+      costo: 1140000,
+      smoke: false,
+      pets: true
+    },
+    {
+      nombre: 'Apartamento luminoso  costas de Miami Beach Biscayne Bay y del ilustre horizonte del Miami Downtown. de Miami',
+      src: 'assets/css/img/img3.jpg',
+      descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+      ubicacion: 'Localizado en el 5333 de la Collins Avenue ',
+      habitaciones: 2,
+      banos: 3,
+      costo: 4000000 ,
+      smoke: true,
+      pets: true
+    },
+    {
+      nombre: 'Apartamento en el centro de la ciudad',
+      src: 'assets/css/img/img4.jpg',
+      descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+      ubicacion: '123 Main Street, Anytown, CA 91234',
+      habitaciones: 2,
+      banos: 2,
+      costo: 2000,
+      smoke: false,
+      pets: true
+    },
+  ]
+  export { propiedades_alquiler };
+  export { propiedades_venta };
